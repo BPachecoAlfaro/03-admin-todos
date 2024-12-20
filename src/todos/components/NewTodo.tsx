64c0966d81751128a7286ccbac6ef66380301f7b
bpachecoalfaro <bpachecoalfaro@gmail.com>
2024-12-20 15:59:26 -0300

    Sección 11 terminada, optimistic y server actions

--- src/todos/components/NewTodo.tsx
+++ src/todos/components/NewTodo.tsx
@@ -2,9 +2,9 @@
 
 import { FormEvent, useState } from "react";
 import { IoTrashOutline } from "react-icons/io5";
-import * as todosApi from '@/todos/helpers/todos';
+// import * as todosApi from '@/todos/helpers/todos';
 import { useRouter } from "next/navigation";
-
+import { addTodo, deleteCompleted } from "../actions/todo-actions";
 
 export const NewTodo = () => {
 
@@ -15,15 +15,16 @@ export const NewTodo = () => {
 	const onSubmit = async (e: FormEvent) => {
 		e.preventDefault();
 		if (description.trim().length === 0) return;
-		const newTodo = await todosApi.createTodo(description);
+
+		await addTodo(description);
 		setDescription('');
-		router.refresh();
+
 	}
 
-	const onDeleteCompleted = async () => {
-		await todosApi.deleteCompletedTodos();
-		router.refresh();
-	}
+	// const onDeleteCompleted = async () => {
+	// 	await deleteCompleted();
+	// 	router.refresh();
+	// }
 
 	return (
 		<form onSubmit={onSubmit} className='flex w-full'>
@@ -40,7 +41,7 @@ export const NewTodo = () => {
 			<span className='flex flex-1'></span>
 
 			<button
-				onClick={() => onDeleteCompleted()}
+				onClick={() => deleteCompleted()}
 				type='button' className="flex items-center justify-center rounded ml-2 bg-red-400 p-2 text-white hover:bg-red-700 transition-all">
 				<IoTrashOutline />
 				<span className="ml-2">Borrar completados</span>
